1ac5ebbe56a3d57cd7f48cccba515954d318a863
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2019-05-10 11:35:22 +0200

    Stop using createFixtureLoader in tests

--- universal-login-relayer/test/helpers/setupWalletService.js
+++ universal-login-relayer/test/helpers/setupWalletService.js
@@ -1,13 +1,13 @@
 import { EventEmitter } from 'fbemitter';
 import sinon from 'sinon';
-import { createFixtureLoader, createMockProvider } from 'ethereum-waffle';
+import { loadFixture } from 'ethereum-waffle';
 import WalletService from '../../lib/services/WalletService';
 import basicWalletContract from '../fixtures/basicWalletContract';
 
 
 export default async function setupWalletService() {
-  const { wallet, walletContract, provider, ensService } = await createFixtureLoader(createMockProvider())(basicWalletContract); // TODO update ethereum-waffle and use loadFixture
-  const hooks = new EventEmitter();
+  const { wallet, walletContract, provider, ensService } = await loadFixture(basicWalletContract);
+  hooks = new EventEmitter();
   const walletService = new WalletService(wallet, null, ensService, hooks, true);
   const callback = sinon.spy();
   hooks.addListener('created', callback);
